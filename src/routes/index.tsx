3a1c8d411c90df0ca9605f2ee4b0bf635de209e4
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-07-03 16:14:59 -0400

    add new route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { OrderCard } from '../pages/OrderCard';
 
 export const AppRoutes = () => (
   <Routes>
     <Route path='/:companyIdURL' element={<Home />} />
     <Route path='/' element={<Home />} />
+    <Route path='/orderCard' element={<OrderCard />} />
   </Routes>
 );
